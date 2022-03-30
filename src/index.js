c7c95d4c5efe273ac7fc7ee15c859e461f765d47
Manjeet <manjeet.ks@evaluationz.com>
2022-03-30 16:14:44 +0530

    Manjeet- UI added for Lazyloading

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ import config from './aws-exports';
 Amplify.configure(config);
 
 ReactDOM.render(
-  <React.StrictMode>
-  <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
